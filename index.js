44fa8374cfa76006f642cb030daaf1ab5f4d2cd6
Dom Sun home <785037725@qq.com>
2016-09-22 23:47:05 +0800

    first server

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 /**
  * Created by ysbpysbp on 2016/9/11.
  */
-//index.js
-require('./app/index')
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs',
+    layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+app.set('view engin', '.hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (request, response) => {
+    response.render('home', {
+    name: 'John'
+})
+});
+
+app.listen(3000)
